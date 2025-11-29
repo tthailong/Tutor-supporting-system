7b947988d92a58dce805c8e3df318bfc5e3c7366
Pham Duy Quang <duyquang@Phams-MacBook-Air.local>
2025-11-29 13:25:53 +0700

    temp: Current changes with conflicts

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,18 @@
 import express from "express"
 import cors from "cors"
 import { connectDB } from "./config/db.js"
-
+import authRouter from "./routes/authRoute.js"
+import sessionRouter from "./routes/sessionRoute.js"
+import tutorRouter from "./routes/tutorRoute.js"
+import studentRouter from "./routes/studentRoute.js"
+import notificationRouter from "./routes/notificationRoute.js";
+import matchingRouter from "./routes/matchingRoutes.js"
+import awardRouter from "./routes/awardRoute.js"
+import userRouter from "./routes/userRoute.js"
+import { errorHandler, notFoundHandler } from "./middleware/errorHandler.js"
+import evaluationRouter from "./routes/evaluationRoutes.js";
+import progressRouter from "./routes/progressRoutes.js";
+import "dotenv/config.js"
 
 // app config
 const app = express()
@@ -13,6 +24,18 @@ app.use(cors())
 
 // db connection
 connectDB();
+
+//api routes
+app.use("/api/auth", authRouter)
+app.use("/api/tutors", tutorRouter);
+app.use("/api/session",sessionRouter)
+app.use("/api/student", studentRouter);
+app.use("/api/notifications", notificationRouter);
+app.use("/api/matching", matchingRouter);
+app.use("/api/awards", awardRouter);
+app.use("/api/users", userRouter);
+app.use("/api/evaluations", evaluationRouter);
+app.use("/api/progress", progressRouter);
 
 app.get("/", (req, res) => {
     res.send("API working")
